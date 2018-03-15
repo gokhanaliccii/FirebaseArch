00cd46f0712a95b67e762885176797cce3fcb954
Gökhan Alıcı <gokhanaliccii@gmail.com>
2018-03-15 23:06:52 +0200

    Firebase realtime database configured

--- app/src/main/java/gokhanaliccii/firebasearch/MainActivity.kt
+++ app/src/main/java/gokhanaliccii/firebasearch/MainActivity.kt
@@ -2,11 +2,17 @@ package gokhanaliccii.firebasearch
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firebaseDB = FirebaseDatabase.getInstance()
+        val userTableRef = firebaseDB.getReference("user")
+
+        userTableRef.setValue("sample", "sdsdds")
     }
 }
